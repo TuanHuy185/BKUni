4f83ded56b3a0891833a656afd16dd5ab9e321c1
Lâm Tuấn Huy <lamtuanhuy1805@gmail.com>
2024-11-04 18:47:22 +0700

    build: create project

---
+++ src/main/java/hcmut/hcmut_spss/HcmutSpssApplication.java
@@ -0,0 +1,13 @@
+package hcmut.hcmut_spss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmutSpssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmutSpssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hcmut/hcmut_spss/HcmutSpssApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmut.hcmut_spss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmutSpssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
